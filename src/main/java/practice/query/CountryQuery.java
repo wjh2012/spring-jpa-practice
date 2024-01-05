ce2bcc8f2f4be31cfb868f81d823e942aa3f565b
wjh <wjh2012@taehwainno.com>
2024-01-05 10:50:51 +0900

    @BatchSize without Join

--- src/main/java/practice/query/CountryQuery.java
+++ src/main/java/practice/query/CountryQuery.java
@@ -5,9 +5,11 @@ import static practice.entity.country.QCountry.country;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import practice.entity.country.City;
 import practice.entity.country.Country;
 
 @Repository
@@ -18,12 +20,27 @@ public class CountryQuery {
     public void test() {
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         
+        List<Country> countries = queryFactory
+            .select(country)
+            .from(country)
+            .fetch();
+
+        List<List<City>> collect = countries.stream().map(Country::getCities)
+            .collect(Collectors.toList());
+        collect.forEach(System.out::println);
+
+        System.out.println("hello test");
+    }
+
+    public void test2() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
         List<Country> fetch = queryFactory
             .select(country)
             .from(country)
             .join(country.cities, city)
             .fetch();
-        
+
         System.out.println("hello test");
     }
 }
