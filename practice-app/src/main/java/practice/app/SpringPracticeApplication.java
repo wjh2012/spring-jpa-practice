6217a44c39c4afb1f76857079662a998ce3a476d
wjh <wjh2012@taehwainno.com>
2024-03-22 14:39:28 +0900

    DBInit, application.yml 추가

--- practice-app/src/main/java/practice/app/SpringPracticeApplication.java
+++ practice-app/src/main/java/practice/app/SpringPracticeApplication.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"practice.jpa"})
+@SpringBootApplication(scanBasePackages = {"practice.jpa"}, exclude = SecurityAutoConfiguration.class)
 @EntityScan(basePackages = {"practice.jpa"})
 @EnableJpaRepositories("practice.jpa")
 public class SpringPracticeApplication {
