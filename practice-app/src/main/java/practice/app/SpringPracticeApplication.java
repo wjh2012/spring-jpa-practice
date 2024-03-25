c6658166b15999b447b86093196428e385981cd3
wjh <wjh2012@taehwainno.com>
2024-03-25 18:08:11 +0900

    chore

--- practice-app/src/main/java/practice/app/SpringPracticeApplication.java
+++ practice-app/src/main/java/practice/app/SpringPracticeApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"practice.jpa",
+    "practice.domain"}, exclude = SecurityAutoConfiguration.class)
 public class SpringPracticeApplication {
 
     public static void main(String[] args) throws IOException {
